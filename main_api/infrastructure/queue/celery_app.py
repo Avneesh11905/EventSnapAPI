6d4c2d797c8d2815584aeabe4f72dd93900fab50
Avneesh119035 <avneeshprojects4257@gmail.com>
2026-08-20 04:01:07 +0530

    feat: remove RabbitMQ configuration and references, switch to Redis for Celery broker

--- main_api/infrastructure/queue/celery_app.py
+++ main_api/infrastructure/queue/celery_app.py
@@ -3,7 +3,7 @@ from config import settings
 
 celery_app = Celery(
     "eventsnap_tasks",
-    broker=settings.RABBITMQ_URL,
+    broker=settings.REDIS_URL,
     backend=settings.REDIS_URL,
     include=["infrastructure.queue.celery_workers"],
 )
@@ -17,4 +17,5 @@ celery_app.conf.update(
     task_track_started=True,
     worker_prefetch_multiplier=1,
     task_acks_late=True,
+    result_expires = 3600
 )
